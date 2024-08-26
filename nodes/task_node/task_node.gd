e88642c7aba1d783d4a25b3fda8a28d5c09d2f9e
francosorbello <franco.sorbelo@gmail.com>
2024-08-26 00:01:40 -0300

    added connections to each slot

--- nodes/task_node/task_node.gd
+++ nodes/task_node/task_node.gd
@@ -2,19 +2,21 @@ extends TodoNode
 
 signal on_add_item_requested(id)
 signal on_title_changed(id, new_title)
-
+var slot_count = 2
 func _on_add_item():
     on_add_item_requested.emit(self.model_id)
 
 func add_item(new_item):
-    $TaskContainer.add_child(new_item)
+    add_child(new_item)
+    slot_count += 1	
+    set_slot(slot_count,true,0,Color.WHITE,true,0,Color.WHITE)
     
 func delete_item(id):
     for item in $TaskContainer.get_children():
         if item.item_id == id:
             item.queue_free()
             break
-            
+
 func set_task_title(_title : String):
     $Title.text = _title
 
